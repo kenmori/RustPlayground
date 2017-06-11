ed7799980018b911490967c040ffb55dec24ae8f
森田賢二 <kenjimoritata@gmail.com>
2017-06-11 18:20:09 +0900

    First compile

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ rustc main.rs
 
 # out
 ./main
+
+--add 2017/06/11
+ref https://kripken.github.io/emscripten-site/docs/tools_reference/emsdk.html#emsdk-howto
+
+cd emsdk-portable
+create .rs file
+
+#compile
+rustc --target=wasm32-unknown-emscripten hello.rs -o hello.html
+rustc --target=wasm32-unknown-emscripten hello.rs -o hello.js
+
--- helloworld/emsdk-portable/emsdk_set_env.sh
+++ helloworld/emsdk-portable/emsdk_set_env.sh
@@ -1,3 +1,4 @@
-export PATH="/Git/rustPlayground/helloworld/emsdk-portable:/Users/moritakenji/.nvm/versions/node/v6.10.2/bin:/Users/moritakenji/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="/Git/rustPlayground/helloworld/emsdk-portable:/Git/rustPlayground/helloworld/emsdk-portable/clang/fastcomp/build_incoming_64/bin:/Git/rustPlayground/helloworld/emsdk-portable/node/4.1.1_64bit/bin:/Git/rustPlayground/helloworld/emsdk-portable/emscripten/incoming:/Users/moritakenji/.nvm/versions/node/v6.10.2/bin:/Users/moritakenji/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export EMSDK="/Git/rustPlayground/helloworld/emsdk-portable"
 export EM_CONFIG="/Users/moritakenji/.emscripten"
+export EMSCRIPTEN="/Git/rustPlayground/helloworld/emsdk-portable/emscripten/incoming"
