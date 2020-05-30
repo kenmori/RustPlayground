eb8e19d0cd4736294a05453c1c2b5b1349253134
kenjimorita <kenjimoritata@gmail.com>
2020-05-30 10:23:55 +0900

    add

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", is_divisible_by(100, 200))
+}
+
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+    lhs % rhs == 0
 }
