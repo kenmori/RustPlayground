3d7af8f392578c559ab507b2f43c510e73c07367
kenjimorita <kenjimoritata@gmail.com>
2020-06-02 23:22:24 +0900

    add

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello World again!")
+}
+
+#[actix_rf::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
