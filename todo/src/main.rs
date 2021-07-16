e732f3917329487516825e03c2f03e6b719a9a6a
kenmori <kenjimoritata@gmail.com>
2021-07-17 08:51:46 +0900

    feat: todo and remove other

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,30 +1,6 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-enum MyError {}
-
-
-impl ResponseError for MyError {}
-
-
-#[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
-}
-
-
-#[actix_rt::main]
-async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
-    Ok(())
-}
-
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
+use thiserror::Error;
 
 struct TodoEntry {
     id: u32,
@@ -37,18 +13,43 @@ struct IndexTemplate {
     entries: Vec<TodoEntry>,
 }
 
-let mut entries = Vec:new()
-entries.push(TodoEntry {
-    id: 1,
-    text: "First entry".to_string(),
-});
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
-entries.push(TodoEntry {
-    id: 2,
-    text: "second entry".to_string()
-})
+impl ResponseError for MyError {}
 
-let html = IndexTemplate { entries }
-let response_body = html.render()?;
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
 
-Ok(HttpResponse::Ok().content_type("text/html").body(response_body))
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+
+    entries.push(TodoEntry {
+        id: 2,
+        text: "second entry".to_string()
+    });
+
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(response_body))
+
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
+}
+
+
+
